cb14a157f089a9c98cf1794b6f25d3a7d8f63632
Şahin Baran Eşsiz <53182048+SahinBaranEssiz@users.noreply.github.com>
2022-08-18 23:47:57 +0300

    new
    errorLogger
    CustomerController

---
+++ Test.UnitTest/CustomerControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace Test.UnitTest
+{
+
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZreo_ReturnNotFound()
+        {
+
+            var controller = new CustomerController();
+            var result = controller.GetCustomer(0);
+
+            //1. yol (kesin Not Found verdiği zaman için)
+            Assert.That(result, Is.TypeOf<NotFound>());
+
+            //2.yol (Not Found ya da türevleri için)
+            //Assert.That(result, Is.InstanceOf<NotFound>());
+
+
+
+        }
+
+        [Test]
+        public void GetCustomer_IdIdnotZero_ReturnOk()
+        {
+            var controller = new CustomerController();
+            var result = controller.GetCustomer(1);
+
+            //1. yol (kesin Ok verdiği zaman için)
+            Assert.That(result, Is.TypeOf<Ok>());
+
+            //2.yol (Ok ya da türevleri için)
+            //Assert.That(result, Is.InstanceOf<Ok>());
+        }
+
+
+
+    }
+}
---
+++ Test.UnitTest/ErrorLoggerTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace Test.UnitTest
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+            logger.Log("a");
+            Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_ThrowArgumentNullException(string error)
+        {
+            var logger = new ErrorLogger();
+
+            
+            Assert.That(() => logger.Log(error), Throws.ArgumentException);
+
+
+        }
+
+    }
+}
