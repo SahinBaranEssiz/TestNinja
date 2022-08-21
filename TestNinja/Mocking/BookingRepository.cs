61920c54142188dbd8d2d798d8deb01abd1cb62a
Şahin Baran Eşsiz <53182048+SahinBaranEssiz@users.noreply.github.com>
2022-08-21 20:56:52 +0300

    new4

---
+++ TestNinja/Mocking/BookingRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+
+namespace TestNinja.Mocking
+{
+    public interface IBookingRepository
+    {
+        IQueryable<Booking> GetActiveBookings(int? excludedBookingID = null);
+    }
+
+    public class BookingRepository : IBookingRepository
+    {
+        public IQueryable<Booking> GetActiveBookings(int? excludedBookingID = null)
+        {
+            var unitOfWork = new UnitOfWork();
+            var bookings =
+                unitOfWork.Query<Booking>()
+                    .Where(
+                        b => b.Status != "Cancelled");
+            if (excludedBookingID.HasValue)
+            
+                bookings = bookings.Where(b => b.Id != excludedBookingID.Value);
+            
+            return bookings;
+        }
+    }
+}
